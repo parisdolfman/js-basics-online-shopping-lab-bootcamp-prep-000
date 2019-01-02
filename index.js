9fe8b4385fad181068f04d40ef101b0d652fac56
Paris Dolfman <parisdolfman@gmail.com>
2019-01-02 19:34:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,10 +58,10 @@ function removeFromCart(item) {
    }
 
 function placeOrder(cardNumber) {
-  if (cardNumber) {
-   return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
-   cart = []
- } else {
-    return `Sorry, we don't have a credit card on file for you.`
+  if (!cardNumber) {
+      console.log("We don't have a credit card on file for you to place your order.");
+    } else {
+      console.log("Your total cost is $" + total() + ", which will be charged to the card " + creditCard + ".");
+      cart = [];
     }
 }
