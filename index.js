9bee6141fd68146a7ff2d1c286b94a91bdde6600
Paris Dolfman <parisdolfman@gmail.com>
2019-01-02 19:21:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber) {
-   return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+   console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
    for (var i = cart.length; i > 0; i--) {
    cart.pop()
  }
