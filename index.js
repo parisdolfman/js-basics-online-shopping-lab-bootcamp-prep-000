68d94e780f931bb75cd451d57da322bfdd101c30
Paris Dolfman <parisdolfman@gmail.com>
2019-01-02 19:38:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,6 +57,6 @@ function removeFromCart(item) {
      return `That item is not in your cart.`
    }
 
-   function placeOrder(cardNumber) {
-
-   }
+function placeOrder(cardNumber) {
+ if (!cardNumber)
+}
